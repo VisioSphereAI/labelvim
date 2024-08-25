29cc0c6d21b696614e4a64b4d0eea2af00f81825
amish0 <amishkumar562@gmail.com>
2024-08-26 04:01:11 +0530

    multiple changes done, 1: in 'labelvim/labelvim/widgets/label_pupop.py' modified pupop class to add object id, when annotated object belongs to any previous annotated object. 2: Modified To handel list of polynomical in segmentation mask for an object, because previously only one polynomial for an object was generated, now segmentation mask of annotated object will be list of polynomial. 3. Modified the save mask according to that.

--- labelvim/labelvim/widgets/label_pupop.py
+++ labelvim/labelvim/widgets/label_pupop.py
@@ -1,19 +1,41 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QListWidget, QPushButton, QMessageBox
+from PyQt5.QtWidgets import QLabel ,QDialog, QVBoxLayout, QHBoxLayout, QLineEdit, QListWidget, QPushButton, QMessageBox, QComboBox
 from PyQt5.QtCore import Qt, pyqtSignal
+from enum import Enum
+from labelvim.utils.config import ANNOTATION_TYPE
 
 class LabelPopup(QDialog):
-    def __init__(self, items, update_label_list_slot_transmitter: pyqtSignal, parent=None):
+    def __init__(self, items: list, data: list, annotation_type: Enum, update_label_list_slot_transmitter: pyqtSignal, parent=None):
         super(LabelPopup, self).__init__(parent)
         
         self.setWindowTitle("Select or Add Item")
         self.setGeometry(100, 100, 300, 400)
-        
+        self.data = data
+        self.items = items
+        self.annotation_type = annotation_type
+        self.selected_item = None
+        self.selected_index = None
+        self.select_id = -1
+        self.id = [-1]
         self.layout = QVBoxLayout(self)
-        
+
+        line_edit_id_combo_layout = QHBoxLayout()
         self.line_edit = QLineEdit(self)
         self.line_edit.setPlaceholderText("Type to filter or add new item")
         self.line_edit.textChanged.connect(self.filter_items)
-        self.layout.addWidget(self.line_edit)
+        line_edit_id_combo_layout.addWidget(self.line_edit)
+        if self.annotation_type == ANNOTATION_TYPE.POLYGON:
+            label = QLabel("ID", self)
+            line_edit_id_combo_layout.addWidget(label)
+            self.id_combo = QComboBox(self)
+            self.update_id_combo()
+            # self.id_combo.addItems([str(i) for i in self.id])
+            line_edit_id_combo_layout.addWidget(self.id_combo)
+        self.layout.addLayout(line_edit_id_combo_layout)
+        
+        # self.line_edit = QLineEdit(self)
+        # self.line_edit.setPlaceholderText("Type to filter or add new item")
+        # self.line_edit.textChanged.connect(self.filter_items)
+        # self.layout.addWidget(self.line_edit)
         
         self.list_widget = QListWidget(self)
         self.list_widget.addItems(items)
@@ -24,16 +46,28 @@ class LabelPopup(QDialog):
         self.add_button.clicked.connect(self.add_item)
         self.layout.addWidget(self.add_button)
         
-        self.items = items
-        self.selected_item = None
-        self.selected_index = None
+        
         self.update_label_list_slot_transmitter = update_label_list_slot_transmitter # Signal to update the label list in the main window
     
+    def update_id_combo(self):
+        self.id_combo.clear()
+        self.id = [str(-1)]
+        # print(self.data["annotations"])
+        self.id = self.id + [str(i['id']) for i in self.data]
+        # print(self.id)
+        self.id_combo.addItems(self.id)
+
     def filter_items(self, text):
         """Filter the items in the list based on the text."""
         self.list_widget.clear()
         filtered_items = [item for item in self.items if text.lower() in item.lower()]
         self.list_widget.addItems(filtered_items)
+        if self.annotation_type == ANNOTATION_TYPE.POLYGON:
+            self.id_combo.clear()
+            self.id = [str(-1)]
+            self.id = self.id + [str(i['id']) for item in filtered_items for i in self.data if item.lower() == self.items[i["category_id"]].lower()]
+            self.id_combo.addItems(self.id)
+        
     
     def add_item(self):
         """Add a new item to the list if it doesn't already exist."""
@@ -52,8 +86,11 @@ class LabelPopup(QDialog):
         """Handle the selection of an item."""
         self.selected_item = item.text()
         self.selected_index = self.items.index(self.selected_item)
+        if self.annotation_type == ANNOTATION_TYPE.POLYGON:
+            self.select_id = int(self.id_combo.currentText())
+        # self.select_id = int(self.id_combo.currentText())
         self.accept()  # Close the dialog and return
     
     def get_selected_item(self):
         """Return the selected item and its index."""
-        return self.selected_item, self.selected_index
+        return self.selected_item, self.selected_index, self.select_id
